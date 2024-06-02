52333365f37c808c79359080ae1e26ab41b7394f
Artuuurr <124526818+Artuuurr@users.noreply.github.com>
2024-06-03 02:59:47 +0500

    first test

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Test from './components/test/test'
+import './css/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<header className='header'>
+			<Test />
+		</header>
+		<main></main>
+		<footer></footer>
+	</React.StrictMode>
 )
