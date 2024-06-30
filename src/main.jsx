fd94b42439262521c6876e68a5c1676c21d5d482
Artur <shakirovartur60@gmail.com>
2024-06-30 13:19:25 +0500

    editimport

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Test from './components/test/test'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 import './css/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<header className='header'>
-			<Test />
-		</header>
-		<main></main>
-		<footer></footer>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>
 )
